c9b35aa2844da3e0f63222a03e238f59b7f5e9f1
657994434 <657994434@qq.com>
2017-11-20 17:38:29 +0800

    基础学习

--- yieldDemo.py
+++ yieldDemo.py
@@ -4,19 +4,37 @@
 
 
 def countdown(n):
-    print ("Counting down from", n)
+    print("Counting down from", n)
     while n >= 0:
         newvalue = (yield n)
-        print('newvalue:',newvalue)
-        print('n:',n)
+        print('newvalue:', newvalue)
+        print('n:', n)
         if newvalue is not None:
             n = newvalue
         else:
             n -= 1
 
-c = countdown(5)
-for x in c:
-    print ('x:',x)
-    if x == 5:
-        c.send(3)
-        # c.send(2)
+
+# c = countdown(5)
+# for x in c:
+#     print ('x:',x)
+#     if x == 5:
+#         c.send(3)
+#         # c.send(2)
+
+n = 0
+
+
+def yieldGen():
+    global n
+    n = n + 1
+    print("start")
+    yield 1
+    print("end")
+
+
+c = yieldGen()
+for i in c:
+    print(i)
+
+print(n)
